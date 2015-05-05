08aedff11d282ba5a0df859fec90f7ae6f0bf587
gcasanova <guillerizo@hotmail.com>
2015-05-05 20:58:54 +0200

    Ignore unknown properties when deserialising FacebookResponse entity

--- src/main/java/auth/domain/entities/FacebookResponse.java
+++ src/main/java/auth/domain/entities/FacebookResponse.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @SuppressWarnings("unused")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FacebookResponse implements Serializable {
 	private static final long serialVersionUID = -3930296634079937670L;
 	
